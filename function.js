5e82e58242ad7f61ba93abe74815cb8b48a5ee74
KimNebreja <nebreja.321612@calamba.sti.edu.ph>
2025-03-29 23:05:43 +0800

    Update function.js

--- function.js
+++ function.js
@@ -95,7 +95,7 @@ async function handleFormSubmit() {
         
         // Store the proofread data in localStorage
         localStorage.setItem('proofreadData', JSON.stringify(data));
-        localStorage.setItem('originalText', JSON.stringify(data.original_text));
+        localStorage.setItem('originalData', JSON.stringify(data.original_text));
         
         // Redirect to proofread page
         window.location.href = 'proofread.html';
